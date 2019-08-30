0e025cd2938c0e42e1846fa205f83bcb57699ed2
ChawnNeal <chawnneal@gmail.com>
2019-08-30 16:16:29 -0500

    implement grid view with random data array

---
+++ metronic_gridview.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="metronic_gridview.aspx.cs" Inherits="metronic_gridview" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <div class="m-portlet">
+        <div class="m-portlet__head">
+			                		<div class="m-portlet__head-caption">
+			<div class="m-portlet__head-title">
+				<h3 class="m-portlet__head-text">
+					Base GridView Data
+				</h3>
+			</div>
+		</div>
+		<div class="m-portlet__head-tools">
+            <button runat="server" id="btnRun" class="btn btn-metal m-btn--icon" title="Search">
+                <i class="fa fa-plus"></i> Search
+            </button>
+		</div>
+		</div>
+        <div class="m-portlet__body">
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+     </div>
+</asp:Content>
+
---
+++ metronic_gridview.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class metronic_gridview : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        btnRun.ServerClick += new EventHandler(this.load_gridview);
+
+
+    }
+    void load_gridview(Object sender, EventArgs e)
+    {
+        int Min = 0;
+        int Max = 20;
+
+        int[] theData = new int[5];
+        Random randNum = new Random();
+        for (int i = 0; i < theData.Length; i++)
+        {
+            theData[i] = randNum.Next(Min, Max);
+        }
+        // int[] theData = new int[] { 14, 17, 5, 11, 2 };
+        GridView1.DataSource = theData.Where(x => x>0).Select((x, index) =>
+    new { RecNo = index + 1, ColumnName = x
+        }).OrderByDescending(x => x.ColumnName).ToList();
+
+        GridView1.DataBind();
+    }
+}
